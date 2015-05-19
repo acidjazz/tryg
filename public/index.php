e8de471fa76cfdefae760406b347c3f2ce5e00f5
kevin olson <acidjazz@gmail.com>
2015-05-19 12:44:39 -0700

    dynamic psr4 loading

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?
 
-require '../vendor/autoload.php';
+$loader = require '../vendor/autoload.php';
+$loader->setPsr4('ctl\\', 'ctl/');
 
 set_error_handler(['\tryg\Debug', 'handler'], E_ALL);
 register_shutdown_function(['\tryg\Debug', 'shutdown']);
@@ -9,3 +10,4 @@ tryg\Debug::def(json_decode(file_get_contents(__DIR__.'/cfg/config.json'), true)
 function hpr() { return call_user_func_array(['\tryg\Debug', 'hpr'], func_get_args()); }
 
 (new tryg\Controller($_SERVER['REQUEST_URI']))->start();
+
