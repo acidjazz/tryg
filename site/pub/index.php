943c94a3d7f580edc60a403e89e35af9b4c06ff1
kevin olson <acidjazz@gmail.com>
2015-05-29 19:58:20 -0700

    made the timezone a tryg config option

--- site/pub/index.php
+++ site/pub/index.php
@@ -7,7 +7,7 @@ set_error_handler(['\tryg\Debug', 'handler'], E_ALL);
 register_shutdown_function(['\tryg\Debug', 'shutdown']);
 
 tryg\Debug::def(json_decode(file_get_contents(__DIR__.'/../cfg/config.json'), true)['cfg']);
-date_default_timezone_set($cfg['timezone']);
+date_default_timezone_set($cfg['tryg']['timezone']);
 
 function hpr() { return call_user_func_array(['\tryg\Debug', 'hpr'], func_get_args()); }
 
