7b29b16a34cfe7df7bf9ed9e245b838f2d52a56e
kevin olson <acidjazz@gmail.com>
2015-05-29 19:13:15 -0700

    added default timezone

--- site/pub/index.php
+++ site/pub/index.php
@@ -7,6 +7,8 @@ set_error_handler(['\tryg\Debug', 'handler'], E_ALL);
 register_shutdown_function(['\tryg\Debug', 'shutdown']);
 
 tryg\Debug::def(json_decode(file_get_contents(__DIR__.'/../cfg/config.json'), true)['cfg']);
+date_default_timezone_set($cfg['timezone']);
+
 function hpr() { return call_user_func_array(['\tryg\Debug', 'hpr'], func_get_args()); }
 
 (new tryg\Controller($_SERVER['REQUEST_URI']))->start();
