33f56e060f39ea86a9dfc10eb776780ecfb568c0
kevin olson <acidjazz@gmail.com>
2016-07-16 20:29:46 -0700

    404 routing

---
+++ site/ctl/Error.php
@@ -0,0 +1,14 @@
+<?
+
+namespace ctl;
+
+class Error {
+
+  public function index() {
+
+    hpr('404 page placeholder');
+
+  }
+
+}
+
--- src/tryg/Controller.php
+++ src/tryg/Controller.php
@@ -24,15 +24,22 @@ class Controller {
   public function start() {
 
     if (!class_exists($this->_controller)) {
-      trigger_error('controller not found: ['.$this->_controller.']');
-      return false;
+      if (!class_exists(self::$_space.'Error')) {
+        trigger_error('controller class not found: "'.$this->_controller.'" nor  "'.self::$_space.'Error" (404 route placeholder)');
+        return false;
+      } else {
+        //(new \ctl\Error())->index();
+        $errorController = self::$_space.'Error';
+        (new $errorController())->index();
+        return false;
+      }
     }
     if (!method_exists($this->_controller, $this->_action) and !method_exists($this->_controller, '__call')) {
       trigger_error('method not found: ['.$this->_controller.'->'.$this->_action.'()]');
       return false;
     }
 
-    call_user_func_array(array(new $this->_controller(),$this->_action), $this->_args);
+    call_user_func_array([new $this->_controller(),$this->_action], $this->_args);
 
   }
 
